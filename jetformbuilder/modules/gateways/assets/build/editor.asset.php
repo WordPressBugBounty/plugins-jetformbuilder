ca05d7b16a4696819dd293ab8127265207519bff
Sean Murphy <sean@iamseanmurphy.com>
2026-07-22 14:19:10 +0000

    3.6.4.1

--- jetformbuilder/modules/gateways/assets/build/editor.asset.php
+++ jetformbuilder/modules/gateways/assets/build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react'), 'version' => '3b999eb237cd2a994483');
+<?php return array('dependencies' => array('react'), 'version' => 'a1823f119d2c497fae6b');
