115e394ffa92640f45a9f71571e0fa8d900e6b52
Sean Murphy <sean@iamseanmurphy.com>
2026-08-05 13:03:21 +0000

    3.6.5

--- jetformbuilder/assets/build/editor/package.asset.php
+++ jetformbuilder/assets/build/editor/package.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '0aefae306822008fa4a6');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '698f06f7dcdf4da19caa');
